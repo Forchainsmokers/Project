a223f9226dee809423065760d127078992ab6918
iHassan <hshabbir94@yahoo.com>
2018-07-13 21:15:30 +0500

    Format code

--- testFile.php
+++ testFile.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>TEST</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
-    
+
     <div class="demo"></div>
     <input type="button" value="">
     <?php
@@ -16,4 +18,5 @@
     $db->get_connection_stats();
      ?>
 </body>
+
 </html>
